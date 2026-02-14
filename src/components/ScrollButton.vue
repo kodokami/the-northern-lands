5c4bf979b7783685fc41f22bda96ddeb3fda2385
kodokami <kodokami@protonmail.com>
2026-02-14 21:45:18 +0100

    scroll buttons

---
+++ src/components/ScrollButton.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="scroll-button" :class="{'active-button': !disabled, 'disabled': disabled}">
+    <slot />
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'ScrollButton',
+    components: {},
+    props: {
+      disabled: Boolean
+    },
+    data() {
+      return {};
+    },
+    methods: {}
+  };
+</script>
+
+<style scoped>
+  .scroll-button {
+    background-image: url("@/assets/buttons/landscape-scroll.png");
+    background-size: 300px 120px;
+    background-repeat: no-repeat;
+    height: 120px;
+    width: 300px;
+    margin-right: 6px;
+    text-align: center;
+    align-content: flex-start;
+    line-height: 114px;
+    font-family: JackPirate;
+    font-size: xx-large;
+    letter-spacing: 3px;
+    color: black;
+    text-shadow: none;
+  }
+  .active-button:hover {
+    cursor: pointer;
+    filter: brightness(1.1);
+    text-shadow: 1px 1px 4px silver;
+  }
+  .disabled {
+    cursor: default !important;
+    filter: brightness(.9) saturate(0);
+  }
+</style>
--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,52 +1,67 @@
 <template>
-  <Banner style="margin-top: 3%;" ref="banner"/>
-  <div class="menu">
-    <div class="link" @click="switchToMap">Mapa</div>
-    <div class="link" @click="switchToGlossary">Glosariusz</div>
-    <div class="link" @click="switchToResources">Materiały</div>   
+  <Banner ref="banner"/>
+  <div class="buttons-container" :class="{'fade-in': !fadeOutButtons, 'fade-out': fadeOutButtons}">
+    <ScrollButton @click="switchToMap">Mapa Świata</ScrollButton>
+    <ScrollButton @click="switchToGlossary" disabled>Glosariusz</ScrollButton>
+    <ScrollButton @click="switchToResources" disabled>Materiały</ScrollButton>   
   </div>
 </template>
 
 <script>
   import router from '@/router/index.js';
   import Banner from '@/components/Banner.vue';
+  import ScrollButton from '@/components/ScrollButton.vue';
 
   export default {
     name: 'MainView',
     components: {
-      Banner
+      Banner,
+      ScrollButton
     },
     data() {
-      return {};
+      return {
+        fadeOutButtons: false
+      };
     },
     methods: {
       switchToMap() {
+        this.fadeOutView();
         this.$refs.banner.hideAndCall(
           () => { router.push('/map'); }
         );
       },
       switchToGlossary() {
-        alert('To be implemented');
+        return;
       },
       switchToResources() {
-        alert('To be implemented');
+        return;
+      },
+      fadeOutView() {
+        this.fadeOutButtons = true;
       }
     }
   };
 </script>
 
 <style scoped>
-  .menu {
+  .buttons-container {
     display: flex;
     flex-wrap: nowrap;
     flex-direction: row;
     justify-content: space-around;
     align-items: center;
+    margin-top: 2rem;
+    margin-bottom: 2rem;
+    width: 1200px;
   }
-  .link {
-    cursor: pointer;
+  @media (width <= 1200px) {
+    .buttons-container {
+      width: 100vw;
+    }
   }
-  .link:hover {
-    color: darkgray;
+  @media (width <= 900px) {
+    .buttons-container {
+      flex-direction: column;
+    }
   }
 </style>
